abfbdda1208d4fdf345a712c7341bc28c024afb0
Marko Dupor <marko.dupor@gmail.com>
2023-01-31 07:41:03 +0100

    Bounding box

--- finger.py
+++ finger.py
@@ -1,5 +1,11 @@
+import numpy as np
+import cv2
+
+
 class Finger:
     def __init__(self, hand_landmarks):
+        self.hand_landmarks = hand_landmarks
+
         self.thumb_tip = hand_landmarks.landmark[4]
         self.thumb_knuckle = hand_landmarks.landmark[2]
 
@@ -51,3 +57,35 @@ class Finger:
 
     def three_fingers_up(self) -> bool:
         return self.thumb_up() and self.index_up() and self.middle_up() and self.ring_down() and self.pinky_down()
+
+    def bounding_box(self, frame):
+
+        # Get the x and y coordinates of the hand landmarks
+        x = []
+        y = []
+        for id, landmark in enumerate(self.hand_landmarks.landmark):
+            x.append(landmark.x)
+            y.append(landmark.y)
+
+
+        # Calculate the bounding box of the hand
+        xmin = np.min(x)
+        ymin = np.min(y)
+        xmax = np.max(x)
+        ymax = np.max(y)
+
+        bounding_box = (xmin, ymin, xmax - xmin, ymax - ymin)
+
+        # Draw the bounding box on the image
+        xmin, ymin, width, height = bounding_box
+        xmax = xmin + width
+        ymax = ymin + height
+
+        print("PIČKA MU MATERINA")
+        print(xmin, ymin, xmax, ymax)
+
+        frame_height, frame_width, _ = frame.shape
+        xmin, ymin, xmax, ymax = [xmin * frame_width, ymin * frame_height, xmax * frame_width, ymax * frame_height]
+        xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
+
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
